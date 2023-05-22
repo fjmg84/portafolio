bb00f790dadf97c317b53a32e5252270478eb3ca
fjmg84 <fjmgqba@gmail.com>
2023-05-22 02:47:17 -0400

    fix error download route

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,8 @@
   import { Router, Route } from "svelte-routing";
 
   import Home from "./Home.svelte";
-  import Download from "./example/Download.svelte";
 </script>
 
 <Router>
   <Route path="/"><Home /></Route>
-  <Route path="/download"><Download /></Route>
 </Router>
