c962552007438e1d37d7c3e68261fd2a7097d7c4
Fidel de J. Miranda Gallego <69002384+fjmg84@users.noreply.github.com>
2023-11-21 18:03:58 -0500

    fix margin ul work experience

--- src/components/main/Main/Jobs.svelte
+++ src/components/main/Main/Jobs.svelte
@@ -29,7 +29,7 @@
       display: flex;
       align-items: center;
       justify-content: left;
-      margin: 2%;
+      margin: 2% 0%;
       span {
         display: flex;
         align-items: center;
